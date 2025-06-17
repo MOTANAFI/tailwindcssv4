526591aa83b542d152a8bcb2851c547e42962e0d
motanafi <twaha2030@gmail.com>
2025-06-17 22:01:33 +0300

    first commit

---
+++ src/Nav.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Nav() {
+  return (
+    <nav className="bg-white p-5 shadow flex items-center justify-between">
+        <div className="text-black">Logo</div>
+        <div className="text-black">main menu</div>
+
+    </nav>
+  )
+}
+
