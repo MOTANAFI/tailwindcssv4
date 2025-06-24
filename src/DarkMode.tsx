d0859c893cbcc91aa5625228681797df7b5f19d1
motanafi <twaha2030@gmail.com>
2025-06-24 19:37:15 +0300

    dark mode toggle completed

--- src/DarkMode.tsx
+++ src/DarkMode.tsx
@@ -1,8 +1,23 @@
 
+import { useEffect, useState } from "react";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { MdOutlineLightMode } from "react-icons/md";
 
 function DarkMode() {
+  const [mode , setMode] = useState("light");
+
+  useEffect(() => {
+    const root = document.body;
+    if(mode === 'dark') {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+  }, [mode])
+
+  const toggleMode = () => {
+    setMode(prevMode => prevMode === 'light' ? 'dark' : 'light');
+  }
  
   return (
    <div className="min-h-screen flex justify-center items-center bg-white dark:bg-gray-900">
@@ -10,10 +25,12 @@ function DarkMode() {
     <div className="dark:text-gray-200">
       <div className="flex justify-between items-center ">
     <h2 className="text-xl font-bold my-2">Heading 1</h2>
-    <div>
-      <MdOutlineDarkMode />
-      <MdOutlineLightMode />
-    </div>
+    <button onClick={toggleMode} className="text-2xl cursor-pointer">
+      {mode === 'light' ? <MdOutlineDarkMode className="text-black"  />:<MdOutlineLightMode className="text-white" />  }
+      
+      
+      
+    </button>
 
       </div>
 
