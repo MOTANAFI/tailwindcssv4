f741c562cc55eaf79d2e849a5e6118fe6cab5809
motanafi <twaha2030@gmail.com>
2025-06-24 18:15:58 +0300

    tailwindcss dark mode

--- src/DarkMode.tsx
+++ src/DarkMode.tsx
@@ -1,21 +1,26 @@
-import { useEffect, useState } from "react";
 
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdOutlineLightMode } from "react-icons/md";
 
 function DarkMode() {
-  const [darkMode, setDarkMode] = useState(localStorage.getItem("darkMode") || "light");
-
-  useEffect(() => {
-    localStorage.setItem("darkMode", darkMode );
-    document.documentElement.classList.toggle("dark", darkMode === 'dark');
-  }, [darkMode]);
-  const toggleDarkMode = () => {
-    setDarkMode(darkMode === 'light' ? 'dark' : 'light');
-
-  }
+ 
   return (
-    <button onClick={toggleDarkMode} className="text-black dark:text-white">
-      {darkMode === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
-    </button>
+   <div className="min-h-screen flex justify-center items-center bg-white dark:bg-gray-900">
+    <div className="rounded-lg w-3/4 p-5 bg-gray-200 dark:bg-gray-800">
+    <div className="dark:text-gray-200">
+      <div className="flex justify-between items-center ">
+    <h2 className="text-xl font-bold my-2">Heading 1</h2>
+    <div>
+      <MdOutlineDarkMode />
+      <MdOutlineLightMode />
+    </div>
+
+      </div>
+
+    <p className="text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum praesentium aspernatur quibusdam numquam ducimus inventore corporis alias nostrum dignissimos voluptates perspiciatis quod error molestiae eos facere odio, quo dolorum omnis.</p>
+    </div>
+    </div>
+   </div>
   )
 }
 
