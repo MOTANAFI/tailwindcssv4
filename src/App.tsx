8089ae7ade08e10c83a6fbd96cd06e2d6044313d
motanafi <twaha2030@gmail.com>
2025-07-11 17:57:56 +0300

    animated background gradient

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@
 // import SlidingMenu from './SlidingMenu'
 // import VerticleMenu from './VerticleMenu'
 // import DarkMode from './DarkMode'
-import Accordion from "./components/accordion"
+// import Accordion from "./components/accordion"
+
+import AnimatedGradientBg from "./components/animatedGradientBg"
 
 
 
@@ -16,7 +18,8 @@ function App() {
      {/* <SlidingMenu /> */}
      {/* <VerticleMenu /> */}
      {/* <DarkMode /> */}
-     <Accordion />
+     {/* <Accordion /> */}
+     <AnimatedGradientBg />
     </>
   )
 }
---
+++ src/components/AnimatedGradientBg.tsx
@@ -0,0 +1,15 @@
+function AnimatedGradientBg() {
+  return (
+    <div className="centerhv">
+      <div className="text-center">
+        <h1 className="text-white">Inovate, Integrate, Inspire</h1>
+        <h3 className="text-purple-300">
+          Driving business transformation with cutting-edge solutions and
+          strategy
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default AnimatedGradientBg;
