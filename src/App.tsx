c9cc50006874329f577d5597191375ca2f3b31a9
motanafi <twaha2030@gmail.com>
2025-07-12 19:33:16 +0300

    animated component background

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
-
 // import Nav from './Nav'
 // import SlidingMenu from './SlidingMenu'
 // import VerticleMenu from './VerticleMenu'
 // import DarkMode from './DarkMode'
 // import Accordion from "./components/accordion"
-
-import AnimatedGradientBg from "./components/animatedGradientBg"
-
-
+// import AnimatedGradientBg from "./components/animatedGradientBg"
+import AnimatedComponentBg from "./components/AnimatedComponentBg";
 
 function App() {
-  
-
   return (
     <>
-     {/* <Nav /> */}
-     {/* <SlidingMenu /> */}
-     {/* <VerticleMenu /> */}
-     {/* <DarkMode /> */}
-     {/* <Accordion /> */}
-     <AnimatedGradientBg />
+      {/* <Nav /> */}
+      {/* <SlidingMenu /> */}
+      {/* <VerticleMenu /> */}
+      {/* <DarkMode /> */}
+      {/* <Accordion /> */}
+      {/* <AnimatedGradientBg /> */}
+      <AnimatedComponentBg />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AnimatedComponentBg.tsx
@@ -0,0 +1,22 @@
+function AnimatedComponentBg() {
+  return (
+    <div className="bg-[url(https://images.unsplash.com/photo-1751420195738-239002789547)] anime-flow">
+      <div className="min-h-screen backdrop-blur-[7rem] centerhv relative">
+        <div className="absolute inset-0 bg-blue-500/40"></div>
+        <div className="bg-white/70 rounded-lg p-5 mx-3 z-10">
+          <h1 className="text-3xl font-semibold mb-3 mx-3">Welcome</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+            laudantium eveniet harum magnam autem, quibusdam veniam recusandae
+            eligendi fugiat sit ea. Nulla nostrum itaque a ea qui optio
+            praesentium reprehenderit error quam porro doloremque sit, veritatis
+            eos in quaerat similique facere sunt id at! Porro nihil quae
+            doloremque sit harum.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AnimatedComponentBg;
